a7d2573045decdac7a2097e152a58c779a54fced
MefedronDestroyer <151124942+MefedronDestroyer@users.noreply.github.com>
2023-11-21 15:46:46 +0300

    Add files via upload

---
+++ 002/002.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    cout << "vvedite predel: ";
+    int x5, c = 0; cin >> x5;
+    for (int q = 0; q <= (x5); q++)
+    {
+        c += q;
+    }
+    cout << c;
+}
---
+++ 003/003.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Razmer massiva: ";
+    int n, s = 0;
+    cin >> n;
+    int* ms = new int[n];
+    cout << "Chisla v massive:" << endl;
+    for (int q = 0; q < n; q++) {
+        cout << q + 1 << " chislo: ";
+        cin >> ms[q];
+    }
+    for (int q = 0; q < n; q++) {
+        cout << ms[q] << " ";
+    }
+    cout << endl;
+    for (int q = 0; q < n; q++) {
+        if (ms[q] % 3 == 0) {
+            s += ms[q];
+        }
+    }
+    cout << "Chisel kratnih 3: " << s << endl;
+    return 0;
+}
